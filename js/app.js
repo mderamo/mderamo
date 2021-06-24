f252f4edc1777cbf3f03a1dcac4a2a880b86b9e5
mderamo <85472327+mderamo@users.noreply.github.com>
2021-06-24 19:08:19 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var canvas=document.getElementById('canvas');
+var ctx=canvas.getContext('2d');
+var rect=canvas.getBoundingClientRect();
+var x=0, y=0, dibujando=false, color='black'; grosor=1;
+
+function defcolor(c){
+    
+}
